f17d113f50d2e6df6797317248f490abcc5f56e0
YoungWik <113011271+YoungWik@users.noreply.github.com>
2022-10-03 08:41:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+//alert("Ala ma kota");
+let promptText = prompt("Co ma ala?", "kota");
+console.log(promptText);
+//let confirmBool = confirm("Czy Ala ma kota?");
+//console.log(confirmBool);
+let promptWrite = prompt("Lorem...", "ipsum");
+document.write(promptWrite);
